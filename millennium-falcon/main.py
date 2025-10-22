774476773d7fe0179233d493cee02d6ed0627894
zrottman <scienceisrobots@gmail.com>
2025-10-21 20:09:53 -0700

    Add code for pulsing led strip

--- millennium-falcon/main.py
+++ millennium-falcon/main.py
@@ -1,6 +1,7 @@
 from machine import Pin
 import time
 import random
+from neopixel import NeoPixel
 
 class LED:
     def __init__(self, pin_num):
@@ -25,6 +26,35 @@ class LED:
             # LED is off: longer intervals
             self.interval = random.randint(1000, 3500)
 
+class PulsingStrip:
+    def __init__(self, pin_num, num_leds, target_color, steps_increasing, steps_decreasing):
+        self.np = NeoPixel(Pin(pin_num), num_leds)
+        self.target_color = target_color
+        self.step_up = tuple(c / steps_increasing for c in target_color)
+        self.step_down = tuple(c / steps_decreasing for c in target_color)
+        self.current_color = [0.0, 0.0, 0.0]
+        self.is_increasing = True
+
+    def update(self):
+        if self.is_increasing:
+            self.current_color = [
+                min(curr + step, target)
+                for curr, step, target in zip(self.current_color, self.step_up, self.target_color)
+            ]
+
+            if all(curr >= target for curr, target in zip(self.current_color, self.target_color)):
+                self.is_increasing = False
+        else:
+            self.current_color = [
+                max(curr - step, 0)
+                for curr, step in zip(self.current_color, self.step_down)
+            ]
+
+            if all(curr <= 0 for curr in self.current_color):
+                self.is_increasing = True
+
+        self.np.fill((int(self.current_color[0]), int(self.current_color[1]), int(self.current_color[2])))
+        self.np.write()
 
 leds = [
     LED(2),
@@ -37,7 +67,10 @@ leds = [
     LED(9)
 ]
 
+pulsing_strip = PulsingStrip(pin_num=15, num_leds=120, target_color=(10, 10, 20), steps_increasing=150, steps_decreasing=20)
+
 while True:
     for led in leds:
         led.update()
+    pulsing_strip.update()
     time.sleep(0.01)
